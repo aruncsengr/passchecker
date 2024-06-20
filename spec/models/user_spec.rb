ce06523cb6bd3465b69867b0e4449abbb2c0e3e6
Arun Kumar <aruncsengr@gmail.com>
2024-06-20 10:36:35 +0530

    test: added test cases to check for inclusion of custom password validator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(10).is_at_most(16) }
+
+    context 'when password with sequential 3 chars' do
+      subject { build(:user, :with_sequential_3_chars) }
+
+      it { should be_invalid }
+    end
+
+    context 'when password with non-sequential 3 chars' do
+      subject { build(:user, :with_non_sequential_3_chars) }
+
+      it { should be_valid }
+    end
   end
 end
