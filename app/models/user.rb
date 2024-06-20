f8729f3c4fb185fea203abf94e3e15b801d9611b
Arun Kumar <aruncsengr@gmail.com>
2024-06-20 10:37:05 +0530

    feat: added custom password validator to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :password, presence: true, length: { minimum: 10, maximum: 16 }
+  validates :password, presence: true, length: { minimum: 10, maximum: 16 }, password: true
 end
