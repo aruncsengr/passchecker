96b856d841e4d091f15befb631a17106d089417e
Arun Kumar <aruncsengr@gmail.com>
2024-06-20 09:37:53 +0530

    test: added test cases for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_db_column(:name) }
+  it { should have_db_column(:password) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(10).is_at_most(16) }
+  end
 end
