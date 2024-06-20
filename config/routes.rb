92c6c183e1dca799a6dc770568e52484c39c090d
Arun Kumar <aruncsengr@gmail.com>
2024-06-20 10:58:09 +0530

    feat: added routes for index and import

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users, only: :index do
+    collection do
+      post :import
+    end
+  end
 end
